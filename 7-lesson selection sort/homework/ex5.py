28c8507f9a682239b3f213373dbeae987ada6064
Diyorbek <desktor00@gmail.com>
2023-12-28 09:22:43 +0500

    ex.5.py

---
+++ 7-lesson selection sort/homework/ex5.py
@@ -0,0 +1,11 @@
+def process_names(names):
+    result = []
+    for name in names:
+        result.append(f"{name} layk bosdi")
+
+    return result
+name_list = ["Diyorbek", "Javohir", "Suxrob"]
+result = process_names(name_list)
+
+for i in result:
+    print(i)
