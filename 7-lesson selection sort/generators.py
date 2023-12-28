561ce6e169de9e9cabc263b29b6038baf33adeff
Diyorbek <desktor00@gmail.com>
2023-12-28 08:10:27 +0500

    lesson code

--- 7-lesson selection sort/generators.py
+++ 7-lesson selection sort/generators.py
@@ -1,13 +1,52 @@
-def square_nums(nums):
+# def square_nums(nums):    
+#     for i in nums:
+#         yield i*i
+# my_list = square_nums([2,4,5,7,9])
+# for i in my_list:
+#     print(i)
     
+# my_list = [x*x for x in [2,4,5,6,7]]
+# print(my_list)
+
+# import random
+# import time
+
+# names = ['Murodjon', 'Boburjon', 'Ikromjon', 'Abdulbosid', 'Javohir']
+# majors = ['Math', 'IT', 'History', 'Geography', 'English','Russian']
+
+# def person_list(people_num):
+#     result = []
     
-    
-    for i in nums:
-        yield i*i
+#     for i in range(people_num):
+#         person = {
+#             'id':i,
+#             'name':random.choice(names),
+#             'major':random.choice(majors)
+#         }
+#         result.append(person)
+        
+#     return result
+
+# def person_generator(people_num):
+#     for i in range(people_num):
+#         person = {
+#             'id':i,
+#             'name':random.choice(names),
+#             'major':random.choice(majors)
+#         }
         
         
-    
+#     yield person
 
-my_list = square_nums([2,4,5,7,9])
 
-print(my_list)
+# time1 = time.time()
+# people_list = person_list(1000000)
+# time2 = time.time()
+
+# time1 = time.time()
+# people_list = person_generator(1000000)
+# time2 = time.time()
+
+
+# print("Funksiya ishlash tezligi {} sekund".format(time2-time1))
+
